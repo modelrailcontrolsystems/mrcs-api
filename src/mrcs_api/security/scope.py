6cd1c25b6bae19b43e2fad9767443f0471da05a6
Bruno Beloff <bruno.beloff@southcoastscience.com>
2026-08-10 19:31:40 +0100

    Added ControlRouterNode publisher identities

--- src/mrcs_api/security/scope.py
+++ src/mrcs_api/security/scope.py
@@ -69,4 +69,4 @@ class Scope(StrEnum, metaclass=MetaEnum):
     # ----------------------------------------------------------------------------------------------------------------
 
     def __str__(self, *args, **kwargs):
-        return f'{self.name}[{self.value}]'
+        return f'{self.name}{{{self.value}}}'
